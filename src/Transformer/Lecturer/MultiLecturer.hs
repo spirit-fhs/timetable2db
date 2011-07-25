933fae547e684c547e05f27f4a64d32d2206e793
Chrisitan Linde <linde.chris@googlemail.com>
2011-07-25 17:33:41 +0200

    update cabal dependencies

--- src/Transformer/Lecturer/MultiLecturer.hs
+++ src/Transformer/Lecturer/MultiLecturer.hs
@@ -8,31 +8,12 @@ data MyMap = MyMap [(String,[String])]
       deriving Read
 --
 --
-type Movies = M.Map String [String]
---
-test = M.insert "name" ["test"] $ M.empty
---
---
-getMapTest = M.lookup "name" test
---
---
-writeMap = do
-    writeFile "test.txt" $ show $ M.toList test 
-    print $ M.toList test
---
---
 readMultiLecturer fileName = do
     daten <- readMap fileName
---    print $ M.lookup "name" $ M.fromList daten
     return $ M.fromList daten
---     where
---       myMap = M.fromList daten
---    case M.fromList daten of
---     fromList 
 --
 --
 readMap :: FilePath -> IO [(String,[String])]
---readMap :: M.Map M.Key M.Value
 readMap fileName = do
     daten <- readFile fileName
     return $ read daten
