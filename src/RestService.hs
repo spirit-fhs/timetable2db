383de6349b68f23bf716b0fd129ee4bc7300d51a
Christian Linde <linde.chris@googlemail.com>
2011-07-28 18:07:36 +0200

    update rest event quest

--- src/RestService.hs
+++ src/RestService.hs
@@ -1,9 +1,28 @@
+{-# LANGUAGE OverloadedStrings #-}
 module RestService where
 --
--- http-wget
-import Network.HTTP.Wget
+import Network.HTTP.Enumerator
+import Network.HTTP.Types
+import qualified Data.ByteString.Lazy as L
 
---main = putStrLn =<< wget "https://www.google.com" [] []
-main = putStrLn =<< wget "https://212.201.64.226:8443/fhs-spirit/event" [] []
+-- cabal install tls http-enumerator
+--
+import Network.TLS (TLSCertificateUsage (CertificateUsageAccept))
+--
+--
+getEventsFromRest :: String -> IO L.ByteString
+getEventsFromRest url = do
+  req0 <- parseUrl $ url
+  let req = req0 { method = methodGet
+                 , requestHeaders = [("Accept", "application/json")]
+                 , checkCerts = const $ return CertificateUsageAccept
+                 }
+  res <- withManager $ httpLbs req
+--  L.putStrLn $ responseBody res
+  return $ responseBody res
+--
+main = do
+  events <- getEventsFromRest "https://212.201.64.226:8443/fhs-spirit/event" 
+  L.putStrLn events
 --
 --
