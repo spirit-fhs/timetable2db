57561e005b7cacd6923bcbd56e006c748b5eb657
Christian Linde <linde.chris@googlemail.com>
2011-08-12 21:02:23 +0200

    add quickCheck

---
+++ tests/TestTest.hs
@@ -0,0 +1,18 @@
+module TestTest where
+--
+-- cabal install QuickCheck
+--
+import Test.QuickCheck
+import Data.List
+--
+--
+qsort :: Ord a => [a] -> [a]
+qsort []     = []
+qsort (x:xs) = qsort lhs ++ [x] ++ qsort rhs
+    where lhs = filter  (< x) xs
+          rhs = filter (>= x) xs
+--
+--
+prop_idempotent xs = qsort (qsort xs) == qsort xs
+--
+--
