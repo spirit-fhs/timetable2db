bafa3d8f1dd17ad6f99e802eca2ba0ff9bf7f920
Christian Linde <linde.chris@googlemail.com>
2011-09-13 07:35:20 +0200

    add time table loader

---
+++ src/LoadTimeTables.hs
@@ -0,0 +1,42 @@
+module LoadTimeTables where
+--
+import HTTPRequest
+import HtmlToListV2
+import Transformer.ListToIS
+import Data.ByteString.Lazy (putStrLn, writeFile)
+import qualified Data.Map as M
+--import Data.List.Split
+--
+import Transformer.TempEventToJSON
+import Transformer.Lecturer.MultiLecturer
+import Transformer.Lecturer.ReadFHSLecturer
+import Transformer.TempEvent.TempEventActions
+import Transformer.AlternativeRoom.AlternativeRoom
+--
+--
+loadTimeTables :: IO ()
+loadTimeTables = do
+  print $ loadTimeTableFromWeb "http://sund.de/steffen/plan/s_bawi6.html" "BaWi6"
+--
+--
+loadTimeTableFromWeb :: String -> String -> IO ()
+loadTimeTableFromWeb uri timeTableName = 
+ do
+  daten <- requestHTML uri
+  transDaten   <- readMultiLecturer multiLecturerFile
+  fhsLecturers <- readJSON          fhsDozentJSON
+  --
+  if ( readAlternativeRoom daten /= [] )
+   then
+    do
+--     print $ roomListToTempEvent $ tail $ tail $ readAlternativeRoom daten
+     print $ generateTempEvents (M.fromList $ (M.toList transDaten) ++ (M.toList fhsLecturers))
+                                (convertListToIS ( tableList' daten ))
+                                (roomListToTempEvent $ tail $ tail $ readAlternativeRoom daten)
+                                timeTableName
+     Data.ByteString.Lazy.writeFile (timeTableName ++ ".json") $ encode $
+         generateTempEvents (M.fromList $ (M.toList transDaten) ++ (M.toList fhsLecturers))
+                            (convertListToIS ( tableList' daten ))
+                            (roomListToTempEvent $ tail $ tail $ readAlternativeRoom daten)
+                            timeTableName
+--
